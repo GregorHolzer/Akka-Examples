acf6a3a9a584f1a121023332fd39a78caa878773
Gregor Holzer <gregor.holzer@student.uibk.ac.at>
2025-09-15 15:28:47 +0200

    setup minimal cluster and script to launch nodes

---
+++ runNode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DEFAULT_PORT_NUM=2551
+DEFAULT_LISTEN_PORT=8000
+DEFAULT_MANAGE_PORT=8558
+NODE_NUM=0
+
+while [ -n "$1" ]; do
+  case "$1" in
+    -n)
+      if [ -z "$2" ]; then
+        echo "Usage: ./runNode.sh -n <number_of_instance>"
+        exit 1
+      fi
+      NODE_NUM=$2
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+PORT_NUM=$((DEFAULT_PORT_NUM + NODE_NUM))
+LISTEN_PORT=$((DEFAULT_LISTEN_PORT + NODE_NUM))
+MANAGE_PORT=$((DEFAULT_MANAGE_PORT + NODE_NUM))
+
+
+echo "Running node $NODE_NUM on port $PORT_NUM, manage via $MANAGE_PORT"
+
+mvn exec:java -Dexec.mainClass=Main -Dakka.remote.artery.canonical.port=$PORT_NUM -Dakka.management.http.port=$MANAGE_PORT
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import akka.actor.ActorSystem;
+import akka.cluster.Cluster;
+import akka.management.javadsl.AkkaManagement;
+
 public class Main {
     public static void main(String[] args) {
-
+        ActorSystem system = ActorSystem.create("RailWayCrossing");
+        Cluster cluster = Cluster.get(system);
+        AkkaManagement.get(system).start();
     }
 }
