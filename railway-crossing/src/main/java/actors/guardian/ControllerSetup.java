4a64b57c5d421fe0d94061ead83cdfc0a8d4fdf3
Gregor Holzer <gregor.holzer@uibk.ac.at>
2025-10-27 17:03:27 +0100

    setup small api to test system

--- railway-crossing/src/main/java/actors/guardian/ControllerSetup.java
+++ railway-crossing/src/main/java/actors/guardian/ControllerSetup.java
@@ -1,6 +1,7 @@
 package actors.guardian;
 
 import actors.controller.Controller;
+import actors.controller_api.ControllerAPI;
 import actors.gate.Gate;
 import actors.light_machine.LightMachine;
 import akka.actor.typed.ActorRef;
@@ -11,6 +12,7 @@ import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 import akka.actor.typed.receptionist.Receptionist;
 import akka.actor.typed.receptionist.ServiceKey;
+import akka.http.javadsl.Http;
 import akka.persistence.typed.PersistenceId;
 
 import java.util.List;
@@ -18,6 +20,10 @@ import java.util.List;
 public class ControllerSetup extends AbstractBehavior<Receptionist.Listing> implements ComponentSetup {
 
     public final static String serviceSuffix = "_Controller";
+
+    public final String serviceId;
+
+    private final String serviceName;
 
     private ActorRef<LightMachine.LightMachineCommand> lightMachine;
 
@@ -31,14 +37,13 @@ public class ControllerSetup extends AbstractBehavior<Receptionist.Listing> impl
 
     private final ServiceKey<Controller.ControllerCommand> controllerServiceKey;
 
-    private final String serviceName;
-
     public static Behavior<Receptionist.Listing> create(String serviceId) {
         return Behaviors.setup(context -> new ControllerSetup(context, serviceId));
     }
 
     private ControllerSetup(ActorContext<Receptionist.Listing> context, String serviceId) {
         super(context);
+        this.serviceId = serviceId;
         this.serviceName = serviceId +  serviceSuffix;
         gateServiceKey = ServiceKey.create(Gate.GateCommand.class, serviceId + GateSetup.serviceSuffix);
         lightMachineServiceKey = ServiceKey.create(LightMachine.LightMachineCommand.class, serviceId + LightMachineSetup.serviceSuffix);
@@ -69,7 +74,20 @@ public class ControllerSetup extends AbstractBehavior<Receptionist.Listing> impl
             controller = getContext().spawn(Controller.create(PersistenceId.ofUniqueId(controllerServiceKey.toString()), gate, lightMachine), String.format("Controller_of_service_%s", serviceName));
             getContext().getSystem().receptionist().tell(Receptionist.register(controllerServiceKey, controller));
             getContext().getLog().info("Controller registered with ServiceKey: {}",  controllerServiceKey);
+            setupAPI();
         }
         return Behaviors.same();
+    }
+
+    private void setupAPI(){
+        ControllerAPI api = new ControllerAPI(controller);
+        int httpPort = getContext().getSystem().settings()
+                .config()
+                .getInt("akka.http.server.default-http-port");
+
+        Http.get(getContext().getSystem())
+                .newServerAt("localhost", httpPort)
+                .bind(api.createRoutes());
+        getContext().getLog().info("Controller of Service {} API bound to {}:{}",serviceId, getContext().getSystem().address(), httpPort);
     }
 }
