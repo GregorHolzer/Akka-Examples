7c11baad93a1cdab16040e86497615a7403973a4
Gregor Holzer <gregor.holzer@uibk.ac.at>
2026-01-27 16:00:05 +0100

    feat: extend services for detailed tracing

--- surveillance-system/services/edgeservices/services.py
+++ surveillance-system/services/edgeservices/services.py
@@ -6,10 +6,15 @@ import uvicorn
 import numpy as np
 
 from fastapi import FastAPI, Request, HTTPException, Response
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.trace import SpanContext, TraceFlags
+from opentelemetry import trace
 
 import json
 import os
-import random
 import base64
 import time
 
@@ -22,6 +27,17 @@ proto = "PROTO" not in os.environ or os.environ["PROTO"].lower() in ["true", "t"
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+resource = Resource(attributes={SERVICE_NAME: "surveillance-iot"})
+trace_provider = TracerProvider(resource=resource)
+span_exporter = OTLPSpanExporter()
+span_processor = BatchSpanProcessor(span_exporter)
+trace_provider.add_span_processor(span_processor)
+trace.set_tracer_provider(trace_provider)
+
+tracer = trace.get_tracer(__name__)
+
+
+
 
 # Utility function for debugging
 # def generate_random_colors(n: int):
@@ -32,81 +48,120 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 #        for _ in range(n)
 #    ]
 
+async def get_parent_context(request: Request):
+    body = await request.body()
+    context_variables = ContextVariable_pb2.ContextVariables()
+    context_variables.ParseFromString(body)
+    traceId = spanId = None
+    for context_variable in context_variables.data:
+        if context_variable.name == "traceId":
+            traceId = context_variable.value.string
+        if context_variable.name == "spanId":
+            spanId = context_variable.value.string
+
+    if traceId and spanId:
+        try:
+            trace_id_int = int(traceId, 16)
+            span_id_int = int(spanId, 16)
+
+            span_ctx = SpanContext(
+                trace_id=trace_id_int,
+                span_id=span_id_int,
+                is_remote=True,
+                trace_flags=TraceFlags(0x01),
+            )
+            parent_context = trace.set_span_in_context(trace.NonRecordingSpan(span_ctx))
+            return parent_context
+        except Exception as e:
+            print("Failed to extract span: " + e.message)
+    return None
 
 @app.post("/detect")
 async def detect(request: Request):
     time_start = time.time_ns() / 1_000_000.0
+    body = await request.body()
+    parent_context = await get_parent_context(request)
 
-    if proto:
-        # Read the raw request body
-        body = await request.body()
+    with tracer.start_as_current_span("detect", context=parent_context) as span:
 
-        # Parse the protobuf message
-        context_variables = ContextVariable_pb2.ContextVariables()
-        context_variables.ParseFromString(body)
+        if proto:
+            # Read the raw request body
 
-        # Extract the image from the protobuf message
-        image_bytes = None
-        for context_variable in context_variables.data:
-            if context_variable.name == "image":
-                image_bytes = context_variable.value.bytes
-                break
-    else:
-        # Parse the request variables
-        context_variables = await request.json()
+            # Parse the protobuf message
+            context_variables = ContextVariable_pb2.ContextVariables()
+            context_variables.ParseFromString(body)
 
-        # Get the image from the request json
-        if "image" in context_variables:
-            image_bytes = base64.b64decode(context_variables["image"].encode("utf-8"))
+            # Extract the image from the protobuf message
+            image_bytes = None
+            for context_variable in context_variables.data:
+                if context_variable.name == "image":
+                    image_bytes = context_variable.value.bytes
+                    break
+        else:
+            # Parse the request variables
+            context_variables = await request.json()
 
-    if image_bytes is None:
-        raise HTTPException(status_code=400, detail="No image provided in the request")
+            # Get the image from the request json
+            if "image" in context_variables:
+                image_bytes = base64.b64decode(context_variables["image"].encode("utf-8"))
 
-    # Read the image
-    np_arr = np.frombuffer(image_bytes, np.uint8)
-    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        if image_bytes is None:
+            raise HTTPException(status_code=400, detail="No image provided in the request")
 
-    if image is None:
-        raise HTTPException(status_code=400, detail="Failed to decode the image")
+        # Read the image
+        np_arr = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    # Perform detection
-    (regions, _) = hog.detectMultiScale(
-        image, winStride=(4, 4), padding=(4, 4), scale=1.05
-    )
+        if image is None:
+            raise HTTPException(status_code=400, detail="Failed to decode the image")
 
-    # For drawing detections:
-    # image_dbg = image.copy()
-    # colors = generate_random_colors(len(regions))
-    # for color, (x, y, w, h) in zip(colors, regions):
-    #    cv2.rectangle(image_dbg, (x, y), (x + w, y + h), color, 2)
-    # cv2.imwrite("image_detected.jpg", image_dbg)
-
-    # Prepare output data
-    if proto:
-        # Create response protobuf message
-        response_context_variables = ContextVariable_pb2.ContextVariables()
-        detections_context_variable = ContextVariable_pb2.ContextVariable(
-            name="hasDetectedPersons",
-            #test: always 0
-            #value=ContextVariable_pb2.Value(bool=False),
-            value=ContextVariable_pb2.Value(bool=len(regions) > 0),
+        # Perform detection
+        (regions, _) = hog.detectMultiScale(
+            image, winStride=(4, 4), padding=(4, 4), scale=1.05
         )
-        response_context_variables.data.append(detections_context_variable)
 
-        # Serialize the response to protobuf format
-        response = response_context_variables.SerializeToString()
-        media_type = "application/x-protobuf"
-    else:
-        response = json.dumps({"hasDetectedPersons": len(regions) > 0})
-        media_type = "application/json"
+        span.set_attribute("hasDetectedPersons", len(regions) > 0)
 
-    time_end = time.time_ns() / 1_000_000.0
+        # For drawing detections:
+        # image_dbg = image.copy()
+        # colors = generate_random_colors(len(regions))
+        # for color, (x, y, w, h) in zip(colors, regions):
+        #    cv2.rectangle(image_dbg, (x, y), (x + w, y + h), color, 2)
+        # cv2.imwrite("image_detected.jpg", image_dbg)
 
-    with open("/tmp/time_detection.csv", "a") as log_file:
-        log_file.write(f"{time_end - time_start}\n")
+        # Prepare output data
+        if proto:
+            # Create response protobuf message
+            response_context_variables = ContextVariable_pb2.ContextVariables()
+            detections_context_variable = ContextVariable_pb2.ContextVariable(
+                name="hasDetectedPersons",
+                # test: always 0
+                # value=ContextVariable_pb2.Value(bool=False),
+                value=ContextVariable_pb2.Value(bool=len(regions) > 0),
+            )
+            trace_context_variable = ContextVariable_pb2.ContextVariable(
+                name="traceId", value=ContextVariable_pb2.Value(string=ctx.trace_id)
+            )
+            span_context_variable = ContextVariable_pb2.ContextVariable(
+                name="spanId", value=ContextVariable_pb2.Value(string=ctx.span_id)
+            )
+            response_context_variables.data.append(detections_context_variable)
+            response_context_variables.data.append(trace_context_variable)
+            response_context_variables.data.append(span_context_variable)
+            # Serialize the response to protobuf format
+            response = response_context_variables.SerializeToString()
+            media_type = "application/x-protobuf"
+        else:
+            response = json.dumps({"hasDetectedPersons": len(regions) > 0})
+            media_type = "application/json"
 
-    # Return the protobuf response
-    return Response(content=response, media_type=media_type)
+        time_end = time.time_ns() / 1_000_000.0
+
+        with open("/tmp/time_detection.csv", "a") as log_file:
+            log_file.write(f"{time_end - time_start}\n")
+
+        # Return the protobuf response
+        return Response(content=response, media_type=media_type)
 
 
 if __name__ == "__main__":
--- surveillance-system/services/iotservices/services.py
+++ surveillance-system/services/iotservices/services.py
@@ -8,6 +8,12 @@ import numpy as np
 from fastapi import FastAPI, HTTPException, Request, Response
 from google.protobuf.json_format import MessageToDict
 from starlette.status import HTTP_200_OK
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.trace import SpanContext, TraceFlags
+from opentelemetry import trace
 
 import base64
 import json
@@ -32,6 +38,15 @@ app = FastAPI()
 # Decide whether protobuf should be used (optional environment variable)
 proto = "PROTO" not in os.environ or os.environ["PROTO"].lower() in ["true", "t", "1"]
 
+resource = Resource(attributes={SERVICE_NAME: "surveillance-iot"})
+trace_provider = TracerProvider(resource=resource)
+span_exporter = OTLPSpanExporter()
+span_processor = BatchSpanProcessor(span_exporter)
+trace_provider.add_span_processor(span_processor)
+trace.set_tracer_provider(trace_provider)
+
+tracer = trace.get_tracer(__name__)
+
 
 # Get the current frame number
 def get_frame_number() -> int:
@@ -53,105 +68,145 @@ def log_hash(data: bytes):
     with open("/tmp/log_send.csv", "a") as log_file:
         log_file.write(log_entry)
 
+async def get_parent_context(request: Request):
+    body = await request.body()
+    context_variables = ContextVariable_pb2.ContextVariables()
+    context_variables.ParseFromString(body)
+    traceId = spanId = None
+    for context_variable in context_variables.data:
+        if context_variable.name == "traceId":
+            traceId = context_variable.value.string
+        if context_variable.name == "spanId":
+            spanId = context_variable.value.string
+
+    if traceId and spanId:
+        try:
+            trace_id_int = int(traceId, 16)
+            span_id_int = int(spanId, 16)
+
+            span_ctx = SpanContext(
+                trace_id=trace_id_int,
+                span_id=span_id_int,
+                is_remote=True,
+                trace_flags=TraceFlags(0x01),
+            )
+            parent_context = trace.set_span_in_context(trace.NonRecordingSpan(span_ctx))
+            return parent_context
+        except Exception as e:
+            print("Failed to extract span: " + e.message)
+    return None
 
 @app.post("/alarm/on")
-async def alarm_on():
-    return Response(status_code=HTTP_200_OK)
+async def alarm_on(request: Request):
+    parent_context = await get_parent_context(request)
+    with tracer.start_as_current_span("alarmOn", context=parent_context):
+        return Response(status_code=HTTP_200_OK)
 
 
 @app.post("/alarm/off")
 async def alarm_on():
-    return Response(status_code=HTTP_200_OK)
+    parent_context = await get_parent_context(request)
+    with tracer.start_as_current_span("alarmOff", context=parent_context):
+        return Response(status_code=HTTP_200_OK)
 
 
 @app.post("/capture")
 async def capture(request: Request):
-    video_number = None
+    with tracer.start_as_current_span("cameraCapture") as span:
+        video_number = None
 
-    # Attempt to read the video number
-    if proto:
-        # Read the raw request body
-        body = await request.body()
+        # Attempt to read the video number
+        if proto:
+            # Read the raw request body
+            body = await request.body()
 
-        # Parse the protobuf message
-        context_variables = ContextVariable_pb2.ContextVariables()
-        context_variables.ParseFromString(body)
+            # Parse the protobuf message
+            context_variables = ContextVariable_pb2.ContextVariables()
+            context_variables.ParseFromString(body)
 
-        # Extract specific values
-        for context_variable in context_variables.data:
-            context_variable_dict = MessageToDict(context_variable)
+            # Extract specific values
+            for context_variable in context_variables.data:
+                context_variable_dict = MessageToDict(context_variable)
 
-            # Check for cameraId and delay
-            if context_variable_dict["name"] == "cameraId":
-                video_number = context_variable_dict["value"].get("integer")
-    else:
-        # Parse the request variables
-        context_variables = await request.json()
+                # Check for cameraId and delay
+                if context_variable_dict["name"] == "cameraId":
+                    video_number = context_variable_dict["value"].get("integer")
+        else:
+            # Parse the request variables
+            context_variables = await request.json()
 
-        # Get context variables from the request json
-        if "cameraId" in context_variables:
-            video_number = int(context_variables["cameraId"])
+            # Get context variables from the request json
+            if "cameraId" in context_variables:
+                video_number = int(context_variables["cameraId"])
 
-    if video_number not in VIDEOS_CAPTURES:
-        raise HTTPException(
-            status_code=400, detail=f"Invalid video_number: {video_number}"
+        if video_number not in VIDEOS_CAPTURES:
+            raise HTTPException(
+                status_code=400, detail=f"Invalid video_number: {video_number}"
+            )
+
+        # Acquire the video frame
+        cap = VIDEOS_CAPTURES[video_number]
+
+        if not cap.isOpened():
+            print(f"Failed to open video {video_number}")
+            raise HTTPException(
+                status_code=400, detail=f"Failed to open video {video_number}"
+            )
+
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_number = get_frame_number() % total_frames
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+
+        ret, frame = cap.read()
+        if not ret:
+            raise HTTPException(status_code=400, detail="Unable to capture frame")
+
+        frame = cv2.resize(frame, (640, 480))
+
+        # Introduce entropy to make every frame unique
+        random_values = (np.random.rand(1, frame.shape[1], frame.shape[2]) * 256).astype(
+            np.uint8
         )
 
-    # Acquire the video frame
-    cap = VIDEOS_CAPTURES[video_number]
+        frame[:1, :, :] = random_values
 
-    if not cap.isOpened():
-        print(f"Failed to open video {video_number}")
-        raise HTTPException(
-            status_code=400, detail=f"Failed to open video {video_number}"
-        )
+        # JPEG compression
+        jpeg_params = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
 
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_number = get_frame_number() % total_frames
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        _, buffer = cv2.imencode(".jpg", frame, jpeg_params)
 
-    ret, frame = cap.read()
-    if not ret:
-        raise HTTPException(status_code=400, detail="Unable to capture frame")
+        buffer_bytes = buffer.tobytes()
 
-    frame = cv2.resize(frame, (640, 480))
+        # Prepare output data
+        if proto:
+            ctx = span.get_span_context()
+            # Create response protobuf message
+            response_context_variables = ContextVariable_pb2.ContextVariables()
+            image_context_variable = ContextVariable_pb2.ContextVariable(
+                name="image", value=ContextVariable_pb2.Value(bytes=buffer_bytes)
+            )
+            trace_context_variable = ContextVariable_pb2.ContextVariable(
+                name="traceId", value=ContextVariable_pb2.Value(string=ctx.trace_id)
+            )
+            span_context_variable = ContextVariable_pb2.ContextVariable(
+                name="spanId", value=ContextVariable_pb2.Value(string=ctx.span_id)
+            )
+            response_context_variables.data.append(image_context_variable)
+            response_context_variables.data.append(trace_context_variable)
+            response_context_variables.data.append(span_context_variable)
+            # Serialize the response to protobuf format
+            response = response_context_variables.SerializeToString()
+            media_type = "application/x-protobuf"
+        else:
+            buffer_base64 = base64.b64encode(buffer_bytes).decode("utf-8")
 
-    # Introduce entropy to make every frame unique
-    random_values = (np.random.rand(1, frame.shape[1], frame.shape[2]) * 256).astype(
-        np.uint8
-    )
+            response = json.dumps({"image": buffer_base64})
+            media_type = "application/json"
 
-    frame[:1, :, :] = random_values
+        log_hash(buffer_bytes)
 
-    # JPEG compression
-    jpeg_params = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
-
-    _, buffer = cv2.imencode(".jpg", frame, jpeg_params)
-
-    buffer_bytes = buffer.tobytes()
-
-    # Prepare output data
-    if proto:
-        # Create response protobuf message
-        response_context_variables = ContextVariable_pb2.ContextVariables()
-        image_context_variable = ContextVariable_pb2.ContextVariable(
-            name="image", value=ContextVariable_pb2.Value(bytes=buffer_bytes)
-        )
-        response_context_variables.data.append(image_context_variable)
-
-        # Serialize the response to protobuf format
-        response = response_context_variables.SerializeToString()
-        media_type = "application/x-protobuf"
-    else:
-        buffer_base64 = base64.b64encode(buffer_bytes).decode("utf-8")
-
-        response = json.dumps({"image": buffer_base64})
-        media_type = "application/json"
-
-    log_hash(buffer_bytes)
-
-    # Return the protobuf response
-    return Response(content=response, media_type=media_type)
+        # Return the protobuf response
+        return Response(content=response, media_type=media_type)
 
 
 if __name__ == "__main__":
